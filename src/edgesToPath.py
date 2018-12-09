d51390c6378ffec5e7e15ca1f63d1f3977acc3df
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2018-12-08 19:52:54 -0600

    Changed to use a set. Now check diagonal direction too

--- src/edgesToPath.py
+++ src/edgesToPath.py
@@ -10,35 +10,43 @@ def dfs(mapvals, checked, i, j):
 	checkedTmp = copy.deepcopy(checked)
 	if (i+1 < len(mapvals)) and (mapvals[i+1][j] == 1) and ((i+1,j) not in checkedTmp):
 		resTmp.append((i+1,j))
-		checkedTmp.append((i+1,j))
+		checkedTmp.add((i+1,j))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i+1, j)
-		checkedTmp += innerChecked
+		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			resTmp += innerRes
+			resTmp += [innerRes]
+	if ((i+1 < len(mapvals)) and (j+1 < len(mapvals[0]))) and (mapvals[i+1][j+1] == 1) and ((i+1,j+1) not in checkedTmp):
+		resTmp.append((i+1,j+1))
+		checkedTmp.add((i+1,j+1))
+		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i+1, j+1)
+		checkedTmp = checkedTmp.union(innerChecked)
+		if (innerRes != [] and innerChecked != []):
+			resTmp += [innerRes]
 	if (j+1 < len(mapvals[0])) and (mapvals[i][j+1] == 1) and ((i, j+1) not in checkedTmp):
 		resTmp.append((i,j+1))
-		checkedTmp.append((i,j+1))
+		checkedTmp.add((i,j+1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i, j+1)
-		checkedTmp += innerChecked
+		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):	
-			resTmp += innerRes		
+			resTmp += innerRes	
 	return (resTmp, checkedTmp)
 
 def getPointsFromEdges(edgemap):
-	checked = []
+	checked = set()
 	res = []
 	for i in xrange(len(edgemap)):
 		for j in xrange(len(edgemap[i])):
 			if ((i,j) not in checked) and (edgemap[i][j] == 1):
-				checked  += [(i,j)]
+				checked.add((i,j))
 				#res += [(i,j)]
 				(resTmp, checkedTmp) = dfs(edgemap, checked, i, j)
 				# add default value to say pick up end-effector
-				checked += checkedTmp
+				checked = checked.union(checkedTmp)
 				if (resTmp != []):
 					res.append([(i,j)] + resTmp)
 				else:
 					res.append([(i,j)])
+	print checked
 	return res
 
 def main():
