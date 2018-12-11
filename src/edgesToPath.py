c0337fa52e8d75b6edc828fdda8fe51373704894
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2018-12-11 12:29:21 -0600

    Cleaned up code a bit, slight optimization on conditionals in dfs

--- src/edgesToPath.py
+++ src/edgesToPath.py
@@ -60,12 +60,10 @@ def getLinesFromPoints(points):
 	elif (n == 1):
 		return (points[0], points[0])
 	else:
-		print points
 		lines = []
 		last = None
 		newPoint = True
 		for p in points:
-			print p
 			if newPoint:
 				last = p
 				newPoint = False
@@ -88,25 +86,6 @@ def plotResults(results):
     		plt.pause(0.0001)                   
    
 	plt.show()
-	# lines = getLinesFromPoints(results[:500])
-	#lc = mc.LineCollection(lines, linewidths=2)
-	# linesSoFar = []
-	# for line in lines:
-	# 	linesSoFar.append(line)
-	# 	lc = mc.LineCollection(linesSoFar, linewidths=2)	
-	# 	fig, ax = pl.subplots()
-	# 	ax.add_collection(lc)
-	# 	ax.autoscale()
-	# 	ax.margins(0.1)
-	# 	fig.show()
-	# 	pl.pause(0.01)
-	# 	pl.close(fig)
-
-	# lines = getLinesFromPoints(results)
-	# for i in range(0, len(results)):
-	# 	((x1,y1),(x2,y2)) = results[i]
-	# 	plt.plot([x1,x2], [y1,y2], 'ro-')
-	# plt.show()
 
 def isAdjacent(l1, l2):
 	(x1, y1) = l1
@@ -123,10 +102,8 @@ def findMostRecentNeighbor(ls, l):
 def separateBranches(ls):
 	last = ls[0]
 	res = [last]
-	print ls
 	for l in ls[1:]:
 		if not isAdjacent(l, last):
-			print l, last
 			res.append((-1,-1))
 			res.append(findMostRecentNeighbor(res, l))
 			res.append(l)
@@ -137,89 +114,70 @@ def separateBranches(ls):
 	return res
 
 def dfs(mapvals, checked, i, j):
-	# right and down, no up and left
-	#print "dfs ", i, j
+	rows = len(mapvals)
+	cols = len(mapvals[0])
 	resTmp = []
 	checkedTmp = copy.deepcopy(checked)
-	if (i-1 >= 0) and (mapvals[i-1][j] > 0) and ((i-1,j) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i-1,j)
+	upOkay = i-1 >= 0
+	downOkay = i+1 < rows
+	rightOkay = j+1 < cols
+	leftOkay = j-1 >= 0
+
+	if upOkay and (mapvals[i-1][j] > 0) and ((i-1,j) not in checkedTmp):
 		resTmp.append((i-1,j))
 		checkedTmp.add((i-1,j))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i-1, j)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes
-	if ((i-1 >= 0) and (j-1 >= 0)) and (mapvals[i-1][j-1] > 0) and ((i-1,j-1) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i-1,j-1)
+	if leftOkay and upOkay and (mapvals[i-1][j-1] > 0) and ((i-1,j-1) not in checkedTmp):
 		resTmp.append((i-1,j-1))
 		checkedTmp.add((i-1,j-1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i-1, j-1)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes
-	if (j-1 >= 0) and (mapvals[i][j-1] > 0) and ((i, j-1) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i,j-1)
+	if leftOkay and (mapvals[i][j-1] > 0) and ((i, j-1) not in checkedTmp):
 		resTmp.append((i,j-1))
 		checkedTmp.add((i,j-1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i, j-1)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):	
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes	
-	if (i+1 < len(mapvals)) and (mapvals[i+1][j] > 0) and ((i+1,j) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i+1,j)
+	if downOkay and (mapvals[i+1][j] > 0) and ((i+1,j) not in checkedTmp):
 		resTmp.append((i+1,j))
 		checkedTmp.add((i+1,j))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i+1, j)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes
-	if ((i+1 < len(mapvals)) and (j+1 < len(mapvals[0]))) and (mapvals[i+1][j+1] > 0) and ((i+1,j+1) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i+1,j+1)
+	if downOkay and rightOkay and (mapvals[i+1][j+1] > 0) and ((i+1,j+1) not in checkedTmp):
 		resTmp.append((i+1,j+1))
 		checkedTmp.add((i+1,j+1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i+1, j+1)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes
-	if (j+1 < len(mapvals[0])) and (mapvals[i][j+1] > 0) and ((i, j+1) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i,j+1)
+	if rightOkay and (mapvals[i][j+1] > 0) and ((i, j+1) not in checkedTmp):
 		resTmp.append((i,j+1))
 		checkedTmp.add((i,j+1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i, j+1)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):	
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes
-	if ((i-1 >= 0) and (j+1 < len(mapvals[0]))) and (mapvals[i-1][j+1] > 0) and ((i-1,j+1) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i-1,j+1)
+	if upOkay and rightOkay and (mapvals[i-1][j+1] > 0) and ((i-1,j+1) not in checkedTmp):
 		resTmp.append((i-1,j+1))
 		checkedTmp.add((i-1,j+1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i-1, j+1)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes
-	if ((i+1 < len(mapvals)) and (j-1 >= 0)) and (mapvals[i+1][j-1] > 0) and ((i+1,j-1) not in checkedTmp):
-		#if resTmp != []:
-			#print (i,j), (i+1,j-1)
+	if downOkay and leftOkay and (mapvals[i+1][j-1] > 0) and ((i+1,j-1) not in checkedTmp):
 		resTmp.append((i+1,j-1))
 		checkedTmp.add((i+1,j-1))
 		(innerRes, innerChecked) = dfs(mapvals, checkedTmp, i+1, j-1)
 		checkedTmp = checkedTmp.union(innerChecked)
 		if (innerRes != [] and innerChecked != []):
-			#resTmp += [(i,j)] + innerRes
 			resTmp += innerRes			
 	return (resTmp, checkedTmp)
 
@@ -254,7 +212,6 @@ def getPointsFromEdges(edgemap):
 		for j in xrange(len(edgemap[i])):
 			if ((i,j) not in checked) and (edgemap[i][j] > 0):
 				checked.add((i,j))
-				#res += [(i,j)]
 				(resTmp, checkedTmp) = dfs(edgemap, checked, i, j)
 				# add default value to say pick up end-effector
 				checked = checked.union(checkedTmp)
