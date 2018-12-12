44b79fcc909a56922ba81b04140b2044df2714da
vnoelifant <veronicamedrano2019@northwester.u.northwestern.edu>
2018-12-11 19:40:05 -0600

    refactored to a function returning cropped image

--- src/face_detect.py
+++ src/face_detect.py
@@ -9,39 +9,42 @@ Created on Tue Dec 11 15:52:46 2018
 import numpy as np
 import cv2 as cv
 
-
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-#eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-
-#load webcam (for later, live webcam)
-# cap = cv.VideoCapture(0)
-
-# Capture frame-by-frame
-# ret, img = cap.read()
-
-img = cv.imread('face.jpg')
-#cv.resize(img,(320,240))
-# convert the baseImage to a gray-based image
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-padding = 20
-
-for f in faces:
-    x,y,w,h = [v for v in f]
-    cv.rectangle(img,(x-padding,y-padding),(x+w+padding,y+h+padding),(255,0,0),2) # blue color
-    # find eyes in region of face
-    #roi_gray = gray[y:y+h, x:x+w]
-    #roi_color = img[y:y+h, x:x+w]
-    #eyes = eye_cascade.detectMultiScale(roi_gray)
-    #for (ex,ey,ew,eh) in eyes:
-        #cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    sub_face = img[y-padding:y+h+padding,x-padding:x+w+padding]
-    face_file = 'face_cropped.jpg'
-    cv.imwrite(face_file,sub_face)
+def face_crop(image):
     
-cv.imshow('face',img)
-# display the window infinitely until any keypress
-cv.waitKey(0)
-cv.destroyAllWindows()
+    # load face training data
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    
+    #read in image
+    img = cv.imread('face.jpg')
+    
+    #resize image if needed
+    #cv.resize(img,(320,240))
+    
+    # convert the baseImage to a gray-based image
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # detect face
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    # add padding to final rectangle to increase its size around face
+    padding = 20
+    
+    # create rectangle for face
+    for f in faces:
+        # define vertices of face rectangle
+        x,y,w,h = [v for v in f]
+        cv.rectangle(img,(x-padding,y-padding),(x+w+padding,y+h+padding),(255,0,0),2) # blue color
+        # create cropped face
+        face = img[y-padding:y+h+padding,x-padding:x+w+padding]
+
+     
+    # show the cropped face
+    cv.imshow('face',face)
+    # display the window infinitely until any keypress
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return face
+
+
+if __name__ == "__main__":
+    face_crop("face.jpg")
